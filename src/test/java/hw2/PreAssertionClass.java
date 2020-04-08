2bf7868cccc1cbaeef3546b1c6f6012f8543f909
Evgeniya Kazakevich <evgeniya_kazakevich@epam.com>
2020-04-08 23:59:03 +0300

    Homework #2 is completed

---
+++ src/test/java/hw2/PreAssertionClass.java
@@ -0,0 +1,52 @@
+package hw2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class PreAssertionClass {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected final String url = "https://jdi-testing.github.io/jdi-light/index.html";
+
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 6);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+    public void openSite(String url) {
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    public void login(String login, String password) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("user-icon"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable((By.id("name")))).sendKeys(login);
+        wait.until(ExpectedConditions.elementToBeClickable((By.id("password")))).sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable((By.id("login-button")))).click();
+    }
+}
+
+
+
---
+++ src/test/java/hw2/ex2/AssertElementsTest.java
@@ -0,0 +1,86 @@
+package hw2.ex2;
+
+import hw2.PreAssertionClass;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class AssertElementsTest extends PreAssertionClass {
+
+    String login = "Roman";
+    String password = "Jdi1234";
+
+    @Test
+    public void checkStepsForExercise2() {
+
+        // 1. Open test site by URL
+        openSite(url);
+        driver.manage().window().maximize();
+
+        //2. Assert Browser title
+        assertEquals(driver.getTitle(), "Home Page");
+
+        // 3. Perform login
+        login(login, password);
+
+        //  4. Assert Username is loggined
+        assertTrue(driver.findElement(By.id("user-name")).isDisplayed());
+        assertEquals(driver.findElement(By.id("user-name")).getText(), "ROMAN IOVLEV");
+
+        // 5. Open through the header menu Service -> Different Elements Page
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Service"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Different elements"))).click();
+        assertEquals(driver.getTitle(), "Different Elements");
+
+        // 6. Select checkboxes
+        WebElement Water = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[normalize-space() = 'Water']")));
+        WebElement Wind = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[normalize-space() = 'Wind']")));
+        Water.click();
+        Wind.click();
+
+        // 7. Select radio
+        WebElement Selen = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[normalize-space() = 'Selen']")));
+        Selen.click();
+
+        // 8. Select in dropdown
+        WebElement ColorsDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".colors select")));
+        Select element = new Select(ColorsDropdown);
+        element.selectByVisibleText("Yellow");
+
+     /* 9. Assert that
+        •	for each checkbox there is an individual log row and value is corresponded to the status of checkbox
+        •	for radio button there is a log row and value is corresponded to the status of radio button
+        •	for dropdown there is a log row and value is corresponded to the selected value.
+
+       */
+        WebElement elementWater = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(text(),'Water: condition changed to true')]")));
+        assertTrue(elementWater.isDisplayed(), "Water checkbox is not displayed in log row");
+        WebElement elementWind = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(text(),'Wind: condition changed to true')]")));
+        assertTrue(elementWind.isDisplayed(), "Wind checkbox is not displayed in log row");
+        WebElement elementSelen = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(text(),'metal: value changed to  Selen')]")));
+        assertTrue(elementSelen.isDisplayed(), "Selen radio button is not displayed in log row");
+        WebElement elementYellow = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(text(),'Colors: value changed to Yellow')]")));
+        assertTrue(elementYellow.isDisplayed(), "Dropdown selected color is not displayed in log row");
+
+    }
+
+}
+
